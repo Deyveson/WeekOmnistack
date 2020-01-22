c4e50ea77188b56df5ba12961c938dab3c004392
Deyveson <deyvesonn@outlook.com>
2020-01-21 23:10:55 -0200

    Comunicando front com api
    Lidando com propriedades reativas e comunicando com o backend

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
 const routes = require('./routes');
 
 const app = express();
@@ -10,6 +11,7 @@ mongoose.connect('mongodb+srv://omnistack:omnistack@cluster0-do39x.mongodb.net/w
 }).then(()=>console.log("DB server connect"))
 .catch(e => console.log("DB error", e));
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
--- frontend/web/src/App.js
+++ frontend/web/src/App.js
@@ -1,4 +1,5 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
+import api from './services/api';
 
 import './global.css';
 import './App.css';
@@ -6,10 +7,19 @@ import './Sidebar.css';
 import './Main.css';
 
 function App() {
+
+  const [devs, setDevs] = useState([]);
+  const [github_username, setGithub_username] = useState('');
+  const [techs, setTechs] = useState('');
+  const [latitude, setLatitude] = useState('');
+  const [longitude, setLongitude] = useState('');
+  
   useEffect(() => {
     navigator.geolocation.getCurrentPosition(
       (position) => {
-        console.log(position)
+        const { latitude, longitude } = position.coords;
+        setLatitude(latitude);
+        setLongitude(longitude);
       },
       (err) => {
         console.log(err)
@@ -18,88 +28,119 @@ function App() {
         timeout: 30000,
       }
     )
+  }, []);
+
+  useEffect(() => {
+
+    async function loadDevs(){
+
+      const response = await api.get('/devs');
+
+      setDevs(response.data);
+
+    } 
+
+    loadDevs();
+
   },[]);
+  // Array vazio - para executar apenas uma vez
+
+  async function handlerAddDev(e){
+
+    e.preventDefault();
+
+    const response = await api.post('/devs', {
+      github_username,
+      techs,
+      latitude,
+      longitude,
+    });
+
+    setGithub_username('');
+    setTechs('');
+
+    setDevs([...devs, response.data]);
+  }
+
   return (
     <div id="app">
+
       <aside>
+
         <strong>Cadastrar</strong>
-        <form>
-          <div className="input-block">
-            <label htmlFor="github_username">Usuário do GitHub</label>
-            <input name="github_username" id="github_username"></input>
-          </div>
 
-          <div className="input-block">
-            <label htmlFor="techs">Tecnologias</label>
-            <input name="techs" id="techs"></input>
-          </div>
+        <form onSubmit={handlerAddDev}>
 
-          <div className="input-group">
             <div className="input-block">
-              <label htmlFor="latitude">Latitude</label>
-              <input name="latitude" id="latitude"></input>
+              <label htmlFor="github_username">Usuário do GitHub</label>
+              <input type=""
+              name="github_username"
+              id="github_username"
+              required
+              value={github_username}
+              onChange={e => setGithub_username(e.target.value)}
+              />
             </div>
 
             <div className="input-block">
-              <label htmlFor="longitude">Longitude</label>
-              <input name="longitude" id="longitude"></input>
+              <label htmlFor="techs">Tecnologias</label>
+              <input type=""
+              name="techs"
+              id="techs"
+              required
+              value={techs}
+              onChange={e => setTechs(e.target.value)}
+              />
             </div>
-          </div>
 
-          <button type="submit">Salvar</button>
+            <div className="input-group">
+
+              <div className="input-block">
+                <label htmlFor="latitude">Latitude</label>
+                <input type="number"
+                 name="latitude"
+                 id="latitude"
+                 required 
+                 value={latitude}
+                 onChange={ e => setLatitude(e.target.value)}
+                />
+              </div>
+
+              <div className="input-block">
+                <label htmlFor="longitude">Longitude</label>
+                <input type="number"
+                name="longitude"
+                id="longitude"
+                required
+                value={longitude}
+                onChange={e => setLongitude(e.target.value)}
+                />
+              </div>
+
+            </div>
+
+            <button type="submit">Salvar</button>
+
         </form>
+
       </aside>
+
       <main>
         <ul>
-          <li className = "dev-item">
+          {devs.map(dev => (
+            <li key={dev._id} className = "dev-item">
             <header>
-              <img src="https://avatars1.githubusercontent.com/u/35642316?s=460&v=4" alt="Deyveson Willian" ></img>
+              <img src={dev.avatar_url} alt={dev.name} ></img>
               <div className="user-info">
-                <strong> Deyveson Willian </strong>
-                <span> ReactJS, React Native, Node.js</span>
+                <strong> { dev.name } </strong>
+                <span> { dev.techs.join(', ') } </span>
               </div>
-           </header>
-           <p> Desenvolvedor FullStack e Apaixonado por tecnologia e desenvolvimento </p>
-           <a href="https://github.com/Deyveson">Acessar perfil no Github</a>
-          </li>
-
-          <li className = "dev-item">
-            <header>
-              <img src="https://avatars1.githubusercontent.com/u/35642316?s=460&v=4" alt="Deyveson Willian" ></img>
-              <div className="user-info">
-                <strong> Deyveson Willian </strong>
-                <span> ReactJS, React Native, Node.js</span>
-              </div>
-           </header>
-           <p> Desenvolvedor FullStack e Apaixonado por tecnologia e desenvolvimento </p>
-           <a href="https://github.com/Deyveson">Acessar perfil no Github</a>
-          </li>
-
-          <li className = "dev-item">
-            <header>
-              <img src="https://avatars1.githubusercontent.com/u/35642316?s=460&v=4" alt="Deyveson Willian" ></img>
-              <div className="user-info">
-                <strong> Deyveson Willian </strong>
-                <span> ReactJS, React Native, Node.js</span>
-              </div>
-           </header>
-           <p> Desenvolvedor FullStack e Apaixonado por tecnologia e desenvolvimento </p>
-           <a href="https://github.com/Deyveson">Acessar perfil no Github</a>
-          </li>
-
-          <li className = "dev-item">
-            <header>
-              <img src="https://avatars1.githubusercontent.com/u/35642316?s=460&v=4" alt="Deyveson Willian" ></img>
-              <div className="user-info">
-                <strong> Deyveson Willian </strong>
-                <span> ReactJS, React Native, Node.js</span>
-              </div>
-           </header>
-           <p> Desenvolvedor FullStack e Apaixonado por tecnologia e desenvolvimento </p>
-           <a href="https://github.com/Deyveson">Acessar perfil no Github</a>
-          </li>
+            </header>
+             <p>{dev.bio}</p>
+             <a href={`https://github.com/${dev.github_username}"`}>Acessar perfil no Github</a>
+            </li>
+          ))}
         </ul>
-
       </main>
     </div>
   );
